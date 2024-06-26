529a9c3e453e29ed1b24e56393fc0ef064322f4a
KISHOREBM <kishorkumar4735@gmail.com>
2024-06-27 00:12:58 +0530

    commit

--- src/assets/Components/Weather.jsx
+++ src/assets/Components/Weather.jsx
@@ -55,7 +55,7 @@ const Weather = () => {
   },[])
   
   useEffect(()=>{
-      if(present==="Partly cloudy"){
+      if(present==="Partly Cloudy"){
         setimagea(cloud)
       }
       else if (present==="Light rain shower" || present==="Mist" ||present==="Patchy rain nearby"){setimagea(drizzle)}
