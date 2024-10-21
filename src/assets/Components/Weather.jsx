c484116059aada3cf9f01ff101ff428db3824e13
KISHOREBM <kishorkumar4735@gmail.com>
2024-10-21 20:08:45 +0530

    commit

--- src/assets/Components/Weather.jsx
+++ src/assets/Components/Weather.jsx
@@ -7,29 +7,40 @@ import wind from '../wind.png'
 import cloud from '../cloud.png'
 import drizzle from '../drizzle.png'
 const Weather = () => {
-  const [cityname,setcity]=useState("delhi")
+  const [cityname,setcity]=useState("koppal")
   const [humidity1,sethumidity]=useState('Null')
   const [windspeed1,setwind]=useState('Null')
   const [temp,settemp]=useState('Null')
   const [present,setpre]=useState("Partly cloudy")
   const [imagea,setimagea]=useState(clearimg)
   const [country,setcount]=useState("Null")
-  const search12=async(cityname)=>{
-    try{
-        const url=`https://api.weatherapi.com/v1/current.json?key=1871891723a74f63b07132925242506&q=${cityname}`
+  const [region, setregion] = useState("")
+  const [searchname,setsearch] = useState("")
+  const search12=async()=>{
+        const url=`https://api.weatherapi.com/v1/current.json?key=1871891723a74f63b07132925242506&q=${searchname}`
         const doitkn=await fetch(url)
         const data = await doitkn.json();
-        console.log(data)
+        if(data.error)
+        {
+          if(data.error['message']==="Parameter q is missing.")
+          {
+            alert("city is missing")
+          }
+          else
+          alert(data.error['message'])
+          
+        }
+        else{
         sethumidity(data['current']["humidity"])
         setwind(data["current"]["wind_kph"])
         settemp(data["current"]["temp_c"])
         setpre(data["current"]["condition"]["text"])
         setcount(data["location"]["country"])
         setcity(data["location"]["name"])
-    }
-    catch (error){
+        setregion(data['location']['region'])
+        }
 
-    }
+
   }
 
   useEffect(()=>{
@@ -37,14 +48,14 @@ const Weather = () => {
       try{
         const url=`https://api.weatherapi.com/v1/current.json?key=1871891723a74f63b07132925242506&q=${cityname}`
         const doitkn=await fetch(url)
-        const data = await doitkn.json();
-        console.log(data)
+        const data = await doitkn.json();      
         sethumidity(data['current']["humidity"])
         setwind(data["current"]["wind_kph"])
         settemp(data["current"]["temp_c"])
         setpre(data["current"]["condition"]["text"])
         setcount(data["location"]["country"])
-        coun=0;
+        setregion(data['location']['region'])
+        setcity(data["location"]["name"])
     }
     catch (error){
 
@@ -61,23 +72,20 @@ const Weather = () => {
       else if (present==="Light rain shower" || present==="Mist" ||present==="Patchy rain nearby"){setimagea(drizzle)}
       else{setimagea(clearimg)}
   },[present])
-  function getit(){
-    let pok=document.querySelector("#val")
-    const newcity=pok.value;
-    setcity(newcity)
-    search12(newcity)
-  }
+
   return (
     <div className='Weather'>
       <div className="search">
-        <input type="text" name="" id="val" placeholder='search'/>
-        <img src={search} alt="" onClick={getit}/>
+        <input type="text" name="" id="val" placeholder='search' value={searchname} onChange={(e)=>{setsearch(e.target.value)}}/>
+        <img src={search} alt="" onClick={()=>{search12()}}/>
       </div>
       <div className='dontkn'>
         <img src={imagea} alt="" className='weatherimg' id="img"/>
         <p className='temp'>{temp}&deg;C</p>
-        <p className='location'>{cityname},{country}</p>
-        <p className="location">{present}</p>
+        <div className='location'>
+          <p >{cityname},{region},{country}</p>
+          <p >{present}</p>
+        </div>
         <div className="weatherdata">
           <div className="col">
             <img src={hymidity} alt="" />
